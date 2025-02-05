e84a7b3f4491e20811a94d47a7fb562a06d4f7d8
happycrying <botisto1111@gmail.com>
2025-02-05 22:39:39 +0100

    Update deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: 'https://oleh-davydov.github.io',
+  base: '/landing',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
